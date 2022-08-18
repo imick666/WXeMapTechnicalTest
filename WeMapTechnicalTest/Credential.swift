e0fccd717346e13cce6476447a945a0dd9fb294a
Mickael Ruzel <mickael.ruzel@gmail.com>
2022-08-18 14:05:55 +0200

    View: Add MapView

---
+++ WeMapTechnicalTest/Credential.swift
@@ -0,0 +1,12 @@
+//
+//  Credential.swift
+//  WeMapTechnicalTest
+//
+//  Created by Mickael Ruzel on 17/08/2022.
+//
+
+import Foundation
+
+enum Credential: String {
+    case mapTileKey = "YOUR API KEY"
+}
---
+++ WeMapTechnicalTest/View/MapView.swift
@@ -0,0 +1,49 @@
+//
+//  MapView.swift
+//  WeMapTechnicalTest
+//
+//  Created by Mickael Ruzel on 17/08/2022.
+//
+
+import SwiftUI
+import Mapbox
+import CoreLocation
+
+struct MapView: UIViewRepresentable {
+    
+    
+    
+    func makeUIView(context: Context) -> MGLMapView {
+        print("Loading")
+        let styleUrl = URL(string: "https://api.maptiler.com/maps/streets/style.json?key=\(Credential.mapTileKey.rawValue)")!
+        let mapView = MGLMapView(frame: .zero, styleURL: styleUrl)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.logoView.isHidden = true
+        mapView.showsScale = true
+        mapView.scaleBarUsesMetricSystem = true
+        
+        mapView.delegate = context.coordinator
+        
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MGLMapView, context: Context) { }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
+    final class Coordinator: NSObject, MGLMapViewDelegate {
+        
+        var control : MapView
+        
+        init(_ control: MapView) {
+            self.control = control
+        }
+        
+        func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
+            
+        }
+        
+    }
+}
