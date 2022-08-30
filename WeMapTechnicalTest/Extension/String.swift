9cc5cb588f2f924ce0237b475280678b99dee087
Mickael Ruzel <mickael.ruzel@gmail.com>
2022-08-30 18:16:43 +0200

    Feature[String]: Add properties to string for convert text from HTML

--- WeMapTechnicalTest/Extension/String.swift
+++ WeMapTechnicalTest/Extension/String.swift
@@ -10,10 +10,11 @@ import UIKit
 
 extension String {
     var fromHtml: String? {
-        guard let data = self.data(using: .utf8),
+        let breaked = self.replacingOccurrences(of: "\n", with: "</br>")
+        guard let data = breaked.data(using: .unicode, allowLossyConversion: true),
               let attributesString = try? NSAttributedString(data: data,
                                                              options: [.documentType: NSAttributedString.DocumentType.html,
-                                                                       .characterEncoding: String.Encoding.utf8.rawValue ],
+                                                                       .characterEncoding: String.Encoding.utf8.rawValue],
                                                              documentAttributes: nil),
               !attributesString.string.isEmpty
         else {
